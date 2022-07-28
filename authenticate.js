fcd7dac9e58910490d4c5748fff3edb35041fb02
Cyd Villavicencio <villacv@gmail.com>
2022-07-28 13:21:03 -0400

    fixed typos

--- authenticate.js
+++ authenticate.js
@@ -4,20 +4,20 @@ const dev = process.env.NODE_ENV !== "production"
 
 exports.COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: true,
-  // secure: !dev,
+  // secure: true,
+  secure: !dev,
   signed: true,
   maxAge: eval(process.env.REFRESH_TOKEN_EXPIRY) * 1000,
   sameSite: "none",
 }
 
-exports.getToken = user => {
+exports.getToken = (user) => {
   return jwt.sign(user, process.env.JWT_SECRET, {
     expiresIn: eval(process.env.SESSION_EXPIRY),
   })
 }
 
-exports.getRefreshToken = user => {
+exports.getRefreshToken = (user) => {
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: eval(process.env.REFRESH_TOKEN_EXPIRY),
   })
